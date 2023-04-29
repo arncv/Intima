01c91df17192ce900142517a6b26b1ec2ace2a63
Arnav <arnavgoel7903@gmail.com>
2023-04-30 04:14:15 +0530

    chatbot remaining

--- server.js
+++ server.js
@@ -3,10 +3,26 @@ const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
 
 
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_KEY ,
-  });
-  const openai = new OpenAIApi(configuration);
+  apiKey: process.env.OPENAI_KEY ,
+});
+const openai = new OpenAIApi(configuration);
+
+// Define a global conversation log
+
+
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
