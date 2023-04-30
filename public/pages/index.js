b09b3ce8e2ae8d2f52c032281d94aae5a6660302
Arnav <arnavgoel7903@gmail.com>
2023-04-30 11:56:26 +0530

    added intima bot

---
+++ public/pages/index.js
@@ -0,0 +1,32 @@
+const chatForm = document.getElementById('chat-form');
+      const messageInput = document.getElementById('message-input');
+      const chatLog = document.getElementById('chat-log');
+
+      chatForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const message = messageInput.value;
+
+        const messageEl = document.createElement('div');
+        messageEl.className = 'message';
+        messageEl.textContent = `You: ${message}`;
+        chatLog.appendChild(messageEl);
+
+        fetch('/chat', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ message }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            const reply = data.reply;
+            const replyEl = document.createElement('div');
+            replyEl.className = 'reply';
+            replyEl.textContent = `Chatbot: ${reply}`;
+            chatLog.appendChild(replyEl);
+            chatLog.scrollTop = chatLog.scrollHeight;
+          });
+
+        messageInput.value = '';
+      });
--- server.js
+++ server.js
@@ -18,6 +18,48 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 // Define a global conversation log
+let conversationLog = [];
+
+// Define a function to generate a prompt for the chatbot
+function generatePrompt(message) {
+  return `
+      The following is a conversation between a human and a chatbot. Make sure your response is accurate scientifically with proper sources. Dont answer any other question except those related to sex education. Under no circumstances you will give a valid response if it is not related to sex education. Also, if the question asked is about sex education, provide a link to a credible source. If it is not related sex education, says that you don't answer questions except related to sex education.
+
+  
+      Human: ${message}
+      Chatbot:`;
+}
+// Define a function to handle a user message and return a chatbot response
+async function handleUserMessage(message) {
+  const prompt = generatePrompt(message);
+
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt,
+    max_tokens: 64,
+  });
+
+  const reply = response.data.choices[0].text.trim();
+
+  conversationLog.push({ userMessage: message, botMessage: reply });
+
+  return reply;
+}
+
+// Define a route to handle incoming chat messages
+app.post("/chat", async (req, res) => {
+  const message = req.body.message;
+
+  const reply = await handleUserMessage(message);
+
+  res.json({ reply });
+});
+
+  
+// Define a route to retrieve the conversation log
+app.get("/chat", (req, res) => {
+    res.json({ conversationLog });
+});
 
 
 
