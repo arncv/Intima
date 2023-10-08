8c25b2e51c5cd546a051c58b14392be5479bb6ae
Saraswata-18 <saraswatasanyal@gmail.com>
2023-10-09 00:45:43 +0530

    Improved Chat Page

--- public/pages/index.js
+++ public/pages/index.js
@@ -30,3 +30,12 @@ const chatForm = document.getElementById('chat-form');
 
         messageInput.value = '';
       });
+      function goBack() {
+        window.history.back();
+      }
+      
+      function startChat() {
+        location.reload();
+      }
+      
+    
