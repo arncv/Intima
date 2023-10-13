0dea65034e9af2fb40255338d15c850cd6baa3d2
unknown <himu0501@gmail.com>
2023-10-14 00:59:40 +0530

    Fixed the navbar, removed the back button as not working neither needed and added margin between navbar and quiz block

--- public/pages/quiz.js
+++ public/pages/quiz.js
@@ -1,3 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
 const quizData=[
     {
         question: "What is the primary goal of sex education?",
